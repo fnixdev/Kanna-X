dd78d9a546660761fc3578ff9bde40ddf8c1f1aa
Yusuke <81726687+yusukesy@users.noreply.github.com>
2021-10-18 17:48:02 -0300

    update alert.py

--- kannax/plugins/utils/alert.py
+++ kannax/plugins/utils/alert.py
@@ -63,7 +63,11 @@ def add_link(website, link):
 def check_link():
 	html = requests.get("https://github.com/fnixdev/Kanna-X/commits/master").content
 	soup = bs(html, "html.parser")
-	link = "https://github.com" + str(soup.p.a.get("href"))
+	try:
+	    url = str(soup.p.a.get("href"))
+	except:
+	    pass
+	link = "https://github.com" + url
 	website = "https://github.com/fnixdev/Kanna-X"
 	if get_link(website) == None:
 		add_link(website, "*") 
