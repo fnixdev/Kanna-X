6227ec1a3e436a285853dbb4795112de9a9c4167
Yusuke <81726687+yusukesy@users.noreply.github.com>
2021-10-18 18:08:45 -0300

    update alert.py

--- kannax/plugins/utils/alert.py
+++ kannax/plugins/utils/alert.py
@@ -63,7 +63,7 @@ def add_link(website, link):
 def check_link():
     html = requests.get("https://github.com/fnixdev/Kanna-X/commits/master").content
     soup = bs(html, "html.parser")
-    if str(soup.p.a) != None:
+    try:
         # lra twste
         link = "https://github.com" + str(soup.p.a.get("href"))
         website = "https://github.com/fnixdev/Kanna-X"
@@ -72,6 +72,8 @@ def check_link():
         if link != get_link(website).link:
             add_link(website, link)
             kannax.bot.send_message(Config.LOG_CHANNEL_ID, f"**Nova atualização disponível**\n\nPara atualizar, use o comando `{Config.CMD_TRIGGER}update -pull`.")
+    except:
+        pass
 
 while True:
     print("kek")
