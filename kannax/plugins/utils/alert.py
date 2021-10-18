6ce69c6aee37bd32a18aee754acb87b147ba17f5
Yusuke <81726687+yusukesy@users.noreply.github.com>
2021-10-18 18:55:47 -0300

    update alert.py

--- kannax/plugins/utils/alert.py
+++ kannax/plugins/utils/alert.py
@@ -73,10 +73,10 @@ def check_link():
     except:
         pass
 
-async def main():
+def main():
     while True:
         check_link()
-        await asyncio.sleep(2)
+        asyncio.sleep(2)
         
 asyncio.ensure_future(main())
 asyncio.get_event_loop().run_forever()
