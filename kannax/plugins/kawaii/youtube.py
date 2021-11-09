76aa4f0defa676be617c9c9a8fd5ac0286c6fead
fnixdev <luisgatn000@gmail.com>
2021-11-09 18:49:45 -0300

    +

--- kannax/plugins/kawaii/youtube.py
+++ kannax/plugins/kawaii/youtube.py
@@ -1,282 +1,162 @@
-# Plugin by https://github.com/RioProjectX/Rio-Music
-# Ported for KannaX by @fnixdev
+# yt_dl module made by @yusukesy|tg @NoteZV
 
-"""novo plugin para ytdl"""
-
-
-from __future__ import unicode_literals
-import asyncio
-import math
 import os
 import time
-import aiofiles
-import aiohttp
+
 import requests
-import yt_dlp
-import wget
-from random import randint
-from urllib.parse import urlparse
-from pyrogram.errors import FloodWait, MessageNotModified
-
-from yt_dlp import YoutubeDL
+from pytube import YouTube
 from youtube_search import YoutubeSearch
-from youtubesearchpython import SearchVideos
 
-from kannax import kannax, Message
-from kannax.utils import time_formatter, humanbytes
+from kannax import Message, kannax
 
-LOGGER = kannax.getLogger(__name__)
-DURATION_LIMIT = int("60")
+
+def search_music(query):
+    result = YoutubeSearch(query, max_results=1).to_dict()
+    return result
+
+
+def get_link(result) -> str:
+    return f"https://www.youtube.com{result[0]['url_suffix']}"
+
+
+def get_filename(result) -> str:
+    title_ = result[0]["title"]
+    title = title_.replace(" ", "_")
+    return title + ".mp3", title + ".mp4"
+
+
+def get_duration(result):
+    duration = result[0]["duration"]
+    secmul, dur, dur_arr = 1, 0, duration.split(":")
+    for i in range(len(dur_arr) - 1, -1, -1):
+        dur += int(dur_arr[i]) * secmul
+        secmul *= 60
+    return duration, dur
+
+
+def get_thumb(result):
+    thumbnail = result[0]["thumbnails"][0]
+    title = result[0]["title"]
+    thumb_name = f"{title}.jpg"
+    thumb = requests.get(thumbnail, allow_redirects=True)
+    open(os.path.join("./kannax/xcache/", thumb_name), "wb").write(thumb.content)
+    return thumb_name
+
+
+def down_song(link, filename):
+    YouTube(link).streams.filter(only_audio=True)[0].download(
+        "./kannax/xcache/", filename=filename
+    )
+
+
+def down_video(link, filename):
+    YouTube(link).streams.get_highest_resolution().download(
+        "./kannax/xcache/", filename=filename
+    )
 
 
 @kannax.on_cmd(
-    "song",
+    "(song"|"music)",
     about={
-        "header": "Music Download",
-        "description": "Baixe musicas usando ytdl",
-        "usage": "{tr}song [nome / reply msg / link]",
+        "header": "Music Downloader",
+        "description": "Baixe músicas usando o pytube. ;-;",
+        "usage": "{tr}song [nome - cantor / reply msg / link]",
     },
 )
-async def song_(message: Message):
-    """baixa musicas do yt"""
-    query = message.input_or_reply_str
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
-    await message.edit("`Processando...`")
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://www.youtube.com{results[0]['url_suffix']}"
-        # print(results)
-        title = results[0]["title"]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"thumb{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        duration = results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
-    except Exception as e:
-        await message.edit("❌ `Som não encontrado`\n\n`Tente inserir um título de música mais claro`")
-        print(str(e))
-        return
-    await message.edit("📥 `Baixando`")
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        rep = f"""
-** Titulo :** __[{title}]({link})__
-** Duração :** __{duration}__
-** Views :** __{results[0]['views']}__
-"""
-        secmul, dur, dur_arr = 1, 0, duration.split(":")
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(dur_arr[i]) * secmul
-            secmul *= 60
-        await message.reply_audio(
-            audio_file,
-            caption=rep,
-            thumb=thumb_name,
-            parse_mode="md",
-            title=title,
-            duration=dur,
-        )
+async def song(message: Message):
+    music = message.input_or_reply_str
+    if not music:
+        await message.edit("`Vou baixar o vento?!`")
+        time.sleep(2)
         await message.delete()
-    except Exception as e:
-        await message.edit("❌ `Ocorreu algum erro, verifique o console.`")
-        print(e)
+        return
+    await message.edit("`Processando...`")
+    result = search_music(music)
+    if result == []:
+        await message.edit("`Não foi possível encontrar a música.`")
+        time.sleep(2)
+        await message.delete()
+        return
+    link = get_link(result)
+    duration, dur = get_duration(result)
+    filename, m = get_filename(result)
+    thumb = get_thumb(result)
     try:
-        os.remove(audio_file)
-        os.remove(thumb_name)
+        down_song(link, filename)
     except Exception as e:
-        print(e)
-
-# Def principais
-
-
-async def progress(current, total, message, start, type_of_ps, file_name=None):
-    now = time.time()
-    diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        if elapsed_time == 0:
-            return
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "{0}{1} {2}%\n".format(
-            "".join("🔴" for i in range(math.floor(percentage / 10))),
-            "".join("🔘" for i in range(10 - math.floor(percentage / 10))),
-            round(percentage, 2),
-        )
-
-        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
-            humanbytes(current), humanbytes(
-                total), time_formatter(estimated_total_time)
-        )
-        if file_name:
+        await message.edit("`Não foi possível baixar a música.`")
+        print(str(e))
+        time.sleep(2)
+        await message.delete()
+    else:
+        if os.path.exists(f"./kannax/xcache/{thumb}"):
+            caption = f"""
+**Título:** __[{result[0]['title']}]({link})__
+**Duração:** __{duration}__
+**Views:** __{result[0]['views']}__
+"""
             try:
-                await message.edit(
-                    "{}\n**File Name:** `{}`\n{}".format(
-                        type_of_ps, file_name, tmp)
+                await message.reply_audio(
+                    audio=f"./kannax/xcache/{filename}",
+                    caption=caption,
+                    title=result[0]["title"],
+                    thumb=f"./kannax/xcache/{thumb}",
+                    duration=dur,
                 )
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-            except MessageNotModified:
-                pass
-        else:
-            try:
-                await message.edit("{}\n{}".format(type_of_ps, tmp))
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-            except MessageNotModified:
-                pass
-
-
-def get_readable_time(seconds: int) -> int:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["d", "m", "j", "hari"]
-    while count < 4:
-        count += 1
-        remainder, result = divmod(
-            seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-    return ping_time
-
-
-ydl_opts = {
-    "format": "bestaudio/best",
-    "writethumbnail": True,
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192",
-        }
-    ],
-}
-
-
-def get_file_extension_from_url(url):
-    url_path = urlparse(url).path
-    basename = os.path.basename(url_path)
-    return basename.split(".")[-1]
-
-
-async def download_song(url):
-    song_name = f"{randint(6969, 6999)}.mp3"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                f = await aiofiles.open(song_name, mode="wb")
-                await f.write(await resp.read())
-                await f.close()
-    return song_name
-
-
-is_downloading = False
-
-
-def time_to_seconds(time):
-    stringt = str(time)
-    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
+                await message.delete()
+            except Exception as e:
+                await message.edit("`Não foi possível enviar a música.`")
+                print(str(e))
+                time.sleep(2)
+                await message.delete()
+            finally:
+                os.remove(f"./kannax/xcache/{filename}")
+                os.remove(f"./kannax/xcache/{thumb}")
 
 
 @kannax.on_cmd(
     "video",
     about={
-        "header": "Video Download",
-        "description": "Baixe videos usando ytdl",
+        "header": "Video Downloader",
+        "description": "Baixe vídeos usando o pytube. ;-;",
         "usage": "{tr}video [nome / reply msg / link]",
     },
 )
-async def video_(message: Message):
-    """baixa videos do youtube"""
-    global is_downloading
+async def video(message: Message):
+    video = message.input_or_reply_str
+    if not video:
+        await message.edit("`Vou baixar o vento?!`")
+        time.sleep(2)
+        await message.delete()
+        return
     await message.edit("`Processando...`")
-    if is_downloading:
-        await message.reply_text(
-            "`Outro download em andamento, tente novamente mais tarde`"
-        )
+    result = search_music(video)
+    if result == []:
+        await message.edit("`Não foi possível encontrar o vídeo.`")
+        time.sleep(2)
+        await message.delete()
         return
-    s_video = message.input_or_reply_str
-    if not s_video:
-        await message.edit("`Syntax error`")
-        return
-    search = SearchVideos(f"{s_video}", offset=1, mode="dict", max_results=1)
-    mi = search.result()
-    mio = mi["search_result"]
-    mo = mio[0]["link"]
-    thum = mio[0]["title"]
-    fridayz = mio[0]["id"]
-    thums = mio[0]["channel"]
-    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
-    await asyncio.sleep(0.6)
-    url = mo
-    sedlyf = wget.download(kekme)
-    opts = {
-        "format": "best",
-        "addmetadata": True,
-        "key": "FFmpegMetadata",
-        "prefer_ffmpeg": True,
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
-        "outtmpl": "%(id)s.mp4",
-        "logtostderr": False,
-        "quiet": True,
-    }
+    link = get_link(result)
+    m, filename = get_filename(result)
     try:
-        is_downloading = True
-        with yt_dlp.YoutubeDL(opts) as ytdl:
-            infoo = ytdl.extract_info(url, False)
-            duration = round(infoo["duration"] / 60)
-
-            if duration > DURATION_LIMIT:
-                await message.edit(
-                    f"`Vídeos com mais de {DURATION_LIMIT} minutos não são permitidos, os vídeos que você deseja baixar têm {duration} minutos`"
-                )
-                is_downloading = False
-                return
-            ytdl_data = ytdl.extract_info(url, download=True)
-
-    except Exception:
-        is_downloading = False
-        return
-
-    c_time = time.time()
-    file_stark = f"{ytdl_data['id']}.mp4"
-    capy = f"**Título ➠** [{thum}]({mo}) \n**Canal ➠ ** {thums}"
-    await message.client.send_video(
-        message.chat.id,
-        video=open(file_stark, "rb"),
-        duration=int(ytdl_data["duration"]),
-        file_name=str(ytdl_data["title"]),
-        thumb=sedlyf,
-        caption=capy,
-        supports_streaming=True,
-        progress=progress,
-        progress_args=(
-            message,
-            c_time,
-            f"`Uploading {s_video}`",
-            file_stark,
-        ),
-    )
-    await message.delete()
-    is_downloading = False
-    for files in (sedlyf, file_stark):
-        if files and os.path.exists(files):
-            os.remove(files)
+        down_video(link, filename)
+    except Exception as e:
+        await message.edit("`Não foi possível baixar o video.`")
+        print(str(e))
+        time.sleep(2)
+        await message.delete()
+    else:
+        caption = f"**Título ➠** __[{result[0]['title']}]({link})__\n**Canal ➠** __{result[0]['channel']}__"
+        try:
+            await message.reply_video(
+                video=f"./kannax/xcache/{filename}",
+                caption=caption,
+            )
+            await message.delete()
+        except Exception as e:
+            await message.reply("`Não foi possível enviar o vídeo.`")
+            print(str(e))
+            time.sleep(2)
+            await message.delete()
+        finally:
+            os.remove(f"./kannax/xcache/{filename}")
