7eb20419d4f53097fa05575da94cf49179fbaa4d
fnixdev <luisgatn000@gmail.com>
2021-11-10 16:53:18 -0300

    remake lastfm

--- kannax/plugins/kawaii/lastfm.py
+++ kannax/plugins/kawaii/lastfm.py
@@ -1,18 +1,20 @@
-"""Last FM"""
-# Copyright (C) 2020 BY KannaX
-# All rights reserved.
-#
-# Authors: 1. https://github.com/lostb053 [TG: @Lostb053]
-#          2. https://github.com/code-rgb [TG: @DeletedUser420]
-#
-# API: https://www.last.fm/api
+# lastfm module by @fnix
 
-import aiohttp
-from kannax import Config, Message, kannax
-from kannax.utils import get_response
+import urllib.parse
+import urllib.request
+import rapidjson as json
+import asyncio
+import httpx
 
-API = "http://ws.audioscrobbler.com/2.0"
+from kannax import kannax, Message, Config
 
+
+BASE_LAST = "http://ws.audioscrobbler.com/2.0"
+LAST_USER = Config.LASTFM_USERNAME
+LAST_KEY = Config.LASTFM_API_KEY
+
+timeout = httpx.Timeout(20)
+http = httpx.AsyncClient(http2=True, timeout=timeout)
 
 @kannax.on_cmd(
     "(lastfm|lt)",
@@ -22,182 +24,41 @@ async def last_fm_pic_(message: Message):
     """now playing"""
     if not await check_lastfmvar(message):
         return
-    params = {
-        "method": "user.getrecenttracks",
-        "limit": 1,
-        "extended": 1,
-        "user": Config.LASTFM_USERNAME,
-        "api_key": Config.LASTFM_API_KEY,
-        "format": "json",
-    }
-    try:
-        view_data = await get_response.json(link=API, params=params)
-    except ValueError:
-        return await message.err("API LastFm inativa", del_in=5)
-    if "error" in view_data:
-        return await message.err(view_data["error"], del_in=5)
-    recent_song = view_data["recenttracks"]["track"]
-    if len(recent_song) == 0:
-        return await message.err("Nenhuma trilha recente encontrada", del_in=5)
-    rep = f"<i><b>{Config.LASTFM_USERNAME} atualmente está ouvindo:</b></i>\n"
-    song_ = recent_song[0]
-    song_name = song_["name"]
-    artist_name = song_["artist"]["name"]
-    image = song_["image"][3].get("#text")
-    rep += f"\n<b>🎶 Musica:</b>  <i>{song_name}</i>\n<b>👥 Artista:</b>  <i>{artist_name}</i>"
-    if song_["loved"] != "0":
-        rep += " (♥️ loved)"
-    # Trying to Fetch Album of the track
-    params_ = {
-        "method": "track.getInfo",
-        "track": song_name,
-        "artist": artist_name,
-        "user": Config.LASTFM_USERNAME,
-        "api_key": Config.LASTFM_API_KEY,
-        "format": "json",
-    }
-    try:
-        view_data_ = await get_response.json(link=API, params=params_)
-    except ValueError:
-        return await message.err("API LastFm inativa", del_in=5)
-    get_track = view_data_["track"]
-    get_scrob = int(get_track["userplaycount"]) + 1
-    scrobbler_ = f"\n\n<b>🎵 {get_scrob} Scrobbles</b>"
-    await message.edit(f"<a href={image}>\u200c</a>" + rep + scrobbler_, parse_mode="html")
-
-
-@kannax.on_cmd(
-    "lastuser",
-    about={
-        "header": "Obter informações do usuário Lastfm",
-        "usage": "{tr}lastuser [lastfm username] (opcional)",
-    },
-)
-async def last_fm_user_info_(message: Message):
-    """user info"""
-    if not await check_lastfmvar(message):
-        return
-    lfmuser = message.input_str or Config.LASTFM_USERNAME
-    await message.edit(f"<code>Obtendo informações do usuario : {lfmuser}</code> ...")
-    params = {
-        "method": "user.getInfo",
-        "user": lfmuser,
-        "api_key": Config.LASTFM_API_KEY,
-        "format": "json",
-    }
-    try:
-        view_data = await get_response.json(link=API, params=params)
-    except ValueError:
-        return await message.err("API LastFm inativa", del_in=5)
-    if "error" in view_data:
-        return await message.err(view_data["error"], del_in=5)
-    lastuser = view_data["user"]
-    if lastuser["gender"] == "m":
-        gender = "🙎‍♂️ "
-    elif lastuser["gender"] == "f":
-        gender = "🙍‍♀️ "
-    else:
-        gender = "👤 "
-    lastimg = lastuser["image"].pop() if len(lastuser["image"]) != 0 else None
-    age = lastuser["age"]
-    playlist = lastuser["playlists"]
-    subscriber = lastuser["subscriber"]
-    result = ""
-    if lastimg:
-        result += f"<a href={lastimg['#text']}>\u200c</a>"
-    result += f"<b>Informações do usuário LastFM para <a href={lastuser['url']}>{lfmuser}</a></b>:\n"
-    result += f" {gender}<b>Nome:</b> {lastuser['realname']}\n"
-    if age != "0":
-        result += f" 🎂 <b>Idade:</b> {age}\n"
-    result += f" 🎵 <b>Total Scrobbles:</b> {lastuser['playcount']}\n"
-    result += f" 🌍 <b>País:</b> {lastuser['country']}\n"
-    if playlist != "0":
-        result += f" ▶️ <b>Playlists:</b> {playlist}\n"
-    if subscriber != "0":
-        result += f" ⭐️ <b>Subscriber:</b> {subscriber}"
-    await message.edit(result, parse_mode="html")
-
-
-@kannax.on_cmd(
-    "lastlove",
-    about={
-        "header": "Obter faixas favoritas do Lastfm",
-        "usage": "{tr}lastlove [lastfm username] (opcional)",
-    },
-)
-async def last_fm_loved_tracks_(message: Message):
-    """liked songs"""
-    if not await check_lastfmvar(message):
-        return
-    user_ = message.input_str or Config.LASTFM_USERNAME
-    await message.edit(f"♥️<code> Buscando faixas favoritas de {user_} ...</code>")
-    params = {
-        "method": "user.getlovedtracks",
-        "limit": 30,
-        "page": 1,
-        "user": user_,
-        "api_key": Config.LASTFM_API_KEY,
-        "format": "json",
-    }
-    try:
-        view_data = await get_response.json(link=API, params=params)
-    except ValueError:
-        return await message.err("API LastFm inativa", del_in=5)
-    tracks = view_data["lovedtracks"]["track"]
-    if "error" in view_data:
-        return await message.err(view_data["error"], del_in=5)
-    if len(tracks) == 0:
-        return await message.edit("Você ainda não tem faixas favoritas.")
-    rep = f"♥️ <b>Faixas favoritas de <a href=https://www.last.fm/user/{user_}>{user_}'s</a></b>"
-    for count, song_ in enumerate(tracks, start=1):
-        song_name = song_["name"]
-        artist_name = song_["artist"]["name"]
-        rep += f"\n{count:02d}. 🎧  <b><a href={song_['url']}>{song_name}</a></b> - <a href={song_['artist']['url']}>{artist_name}</a>"
-    await message.edit(rep, disable_web_page_preview=True, parse_mode="html")
-
-
-@kannax.on_cmd(
-    "lastplayed",
-    about={
-        "header": "Obtenha músicas LastFm tocadas recentemente",
-        "usage": "{tr}lastplayed [lastFM username] (opcional)",
-    },
-)
-async def last_fm_played_(message: Message):
-    """recently played songs"""
-    await message.edit(
-        "<code> 🎵 Buscando músicas reproduzidas recentemente de last.fm ...</code>"
+    resp = await http.get(
+        f"{BASE_LAST}?method=user.getrecenttracks&limit=3&extended=1&user={LAST_USER}&api_key={LAST_KEY}&format=json"
     )
-    if not await check_lastfmvar(message):
+    if not resp.status_code == 200:
+        await message.reply("__Algo deu errado__")
         return
-    user_ = message.input_str or Config.LASTFM_USERNAME
-    params = {
-        "method": "user.getrecenttracks",
-        "limit": 30,
-        "extended": 1,
-        "user": user_,
-        "api_key": Config.LASTFM_API_KEY,
-        "format": "json",
-    }
     try:
-        view_data = await get_response.json(link=API, params=params)
-    except ValueError:
-        return await message.err("API LastFm inativa", del_in=5)
-    if "error" in view_data:
-        return await message.err(view_data["error"], del_in=5)
-    recent_song = view_data["recenttracks"]["track"]
-    if len(recent_song) == 0:
-        return await message.err("Nenhuma trilha recente encontrada", del_in=5)
-    rep = f"<b><a href=https://www.last.fm/user/{user_}>{user_}'s</a></b> musicas tocadas recentemente:"
-    for count, song_ in enumerate(recent_song, start=1):
-        song_name = song_["name"]
-        artist_name = song_["artist"]["name"]
-        rep += f"\n{count:02d}. 🎧  <b><a href={song_['url']}>{song_name}</a></b> - <a href={song_['artist']['url']}>{artist_name}</a>"
-        if song_["loved"] != "0":
-            rep += " ♥️"
-    await message.edit(rep, disable_web_page_preview=True, parse_mode="html")
-
-
+        first_track = resp.json().get("recenttracks").get("track")[0]
+    except IndexError:
+        await message.edit("Você não scrobblou nenhuma música...")
+        return
+    image = first_track.get("image")[3].get("#text")
+    artist = first_track.get("artist").get("name")
+    artist_ = urllib.parse.quote(artist)
+    song = first_track.get("name")
+    song_ = urllib.parse.quote(song)
+    loved = int(first_track.get("loved"))
+    fetch = await http.get(
+        f"{BASE_LAST}?method=track.getinfo&artist={artist_}&track={song_}&user={LAST_USER}&api_key={LAST_KEY}&format=json"
+    )
+    info = json.loads(fetch.content)
+    last_user = info["track"]
+    get_scrob = int(last_user["userplaycount"]) + 1
+    rep = f"**{LAST_USER} esta ouvindo:**\n\n"
+    if not loved:
+        rep += f"<b>🎶 Musica:</b>  <i>{song}</i>\n<b>👥 Artista:</b>  <i>{artist}</i>"
+    else:
+        rep += f"<b>🎶 Musica:</b>  <i>{song}</i> ❤️(loved)\n<b>👥 Artista:</b>  <i>{artist}</i>"
+    if get_scrob:
+        rep += f"\n\n__📊 {get_scrob} scrobbles__"
+    if image:
+        rep += f"<a href='{image}'>\u200c</a>"
+    await message.edit(rep)
+    
+    
 async def check_lastfmvar(message: Message):
     if hasattr(Config, "LASTFM_API_KEY") and (
         Config.LASTFM_API_KEY and Config.LASTFM_USERNAME
@@ -207,49 +68,3 @@ async def check_lastfmvar(message: Message):
         "**LastFm Config Vars não encontradas !\n Veja este [Guia](https://fnixdev.gitbook.io/kannax/variaveis-necessarias-para-kannax/vars_opcionais/lastfm) para mais informações.**"
     )
     return False
-
-# The code i am using at the moment, this might work as it is, feel free to edit as per bot's use
-
-du = "https://last.fm/user/"
-
-
-async def resp(params: dict):
-    async with aiohttp.ClientSession() as session, \
-            session.get("http://ws.audioscrobbler.com/2.0", params=params) as res:
-        return res.status, await res.json()
-
-
-async def recs(query, typ, lim):
-    params = {"method": f"user.get{typ}", "user": query, "limit": lim,
-              "api_key": Config.LASTFM_API_KEY, "format": "json"}
-    return await resp(params)
-
-USERNAME = Config.LASTFM_USERNAME
-
-
-@kannax.on_cmd(
-    "compat",
-    about={
-        "header": "Compat",
-        "description": "verifique o nível de compatibilidade de música com outros usuários de lastfm",
-        "usage": "{tr}compat username ou {tr}compat username1|username2",
-    },
-)
-async def lastfm_compat_(message: Message):
-    """Mostra compatibilidade musical"""
-    await message.edit(f"Processando...")
-    msg = message.input_str
-    if not msg:
-        return await message.edit("Pls verifique `{tr}help compat`")
-    diff = "|" in msg
-    us1, us2 = msg.split("|") if diff else USERNAME, msg
-    ta = "topartists"
-    ta1 = (await recs(us1, ta, 500))[1][ta]["artist"]
-    ta2 = (await recs(us2, ta, 500))[1][ta]["artist"]
-    ad1, ad2 = [n["name"] for n in ta1], [n["name"] for n in ta2]
-    display = f"****[{us1}]({du}{us1})**** e **[{us2}]({du}{us2})**"
-    comart = [value for value in ad2 if value in ad1]
-    disart = ", ".join({comart[r] for r in range(min(len(comart), 5))})
-    compat = min((len(comart) * 100 / 40), 100)
-    rep = f"{display} ambos ouvem: \n__{disart}__...\n\nCompatibilidade músical é de **{compat}%**"
-    await message.edit(rep, disable_web_page_preview=True)
