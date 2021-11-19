65cdba0027681fc350976e5e1226fc739aa026b9
fnixdev <luisgatn000@gmail.com>
2021-11-19 17:16:29 -0300

    alguns ajustes

--- kannax/plugins/fun/kang.py
+++ kannax/plugins/fun/kang.py
@@ -16,7 +16,7 @@ from pyrogram.raw.functions.messages import GetStickerSet
 from pyrogram.raw.types import InputStickerSetShortName
 
 from kannax import Config, Message, kannax
-from kannax.utils import get_response
+from kannax.utils import get_response, media_to_image
 
 
 @kannax.on_cmd(
@@ -325,3 +325,27 @@ async def sticker_search(message: Message):
     if not sticker_pack:
         sticker_pack = "`❌ Não encontrado!`"
     await message.edit((titlex + sticker_pack), disable_web_page_preview=True)
+
+
+# import from oub-remix to ux by Itachi_HTK/ashwinstr
+
+@kannax.on_cmd(
+    "imgs",
+    about={
+        "header": "Converta para imagem",
+        "description": "Converta GIF/sticker/vídeo/thumbnail de música em imagem no formato jpg",
+        "usage": "{tr}imgs [responda a uma mídia]",
+    },
+)
+async def img(message: Message):
+    if not message.reply_to_message:
+        await message.edit("Responda a uma mídia...", del_in=5)
+        return
+    reply_to = message.reply_to_message.message_id
+    await message.edit("Convertendo...", del_in=5)
+    file_name = "kanna_convert.jpg"
+    down_file = os.path.join(Config.DOWN_PATH, file_name)
+    if os.path.isfile(down_file):
+        os.remove(down_file)
+    image = await media_to_image(message)
+    await message.reply_photo(image, reply_to_message_id=reply_to)
