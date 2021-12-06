efb7ad3d577ca4b43fcbd6deeb2a31a5379a6fe1
fnixdev <luisgatn000@gmail.com>
2021-12-06 01:58:51 -0300

    ++

--- kannax/plugins/help.py
+++ kannax/plugins/help.py
@@ -84,7 +84,7 @@ async def _init() -> None:
         Config.NEW_ALIVE_MEDIA = media_["url"]
         Config.ALIVE_MEDIA_TYPE = media_["type"]
     else:
-        Config.NEW_ALIVE_MEDIA = "https://telegra.ph/file/1fb4c193b5ac0c593f528.jpg"
+        Config.NEW_ALIVE_MEDIA = "https://telegra.ph/file/4e956ef52c931570fb110.png"
         Config.ALIVE_MEDIA_TYPE = "photo"
 
 
