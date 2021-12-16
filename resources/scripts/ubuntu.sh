e4cfea9e1cea44b9f31d69e0028d5515ef61a4be
fnixdev <luisgatn000@gmail.com>
2021-12-16 13:09:11 -0300

    add weather beta

--- resources/scripts/ubuntu.sh
+++ resources/scripts/ubuntu.sh
@@ -14,22 +14,18 @@ echo "Instalando Dependencias.."
 if [ ! -n "`which sudo`" ]; then
   apt-get update && apt-get install sudo -y
 fi
+
 sudo apt-get update -y
-sudo apt-get install tree -y
-sudo apt-get install wget2 -y
-sudo apt-get install p7zip-full -y
-sudo apt-get install jq -y
-sudo apt-get install ffmpeg -y
-sudo apt-get install wget -y
-sudo apt-get install git -y
+sudo apt-get install tree wget2 p7zip-full jq ffmpeg wget git neofetch -y
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm google-chrome-stable_current_amd64.deb
+
 echo "Instalando Python3.9..."
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.9 -y
-sudo apt install neofetch -y
+sudo apt install python3-pip
 
 echo "Clonando Repositorio."
 cd ~
@@ -37,9 +33,8 @@ sudo git clone https://github.com/fnixdev/Kanna-X.git
 cd Kanna-X
 
 echo "Instalando Requisitos do KannaX."
-sudo pip install -r requirements.txt
 sudo pip3 install -r requirements.txt
-sudo cp config.env.sample config.env
+
 echo "Configurando Screen ..."
 
 sudo apt install screen -y
