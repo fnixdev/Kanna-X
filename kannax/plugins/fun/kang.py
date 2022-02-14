6fa340d47a22ab856bfa3ab29c9a3e2fef62d610
fnixdev <luisgatn000@gmail.com>
2022-02-14 00:45:14 -0300

    Add sticker video

--- kannax/plugins/fun/kang.py
+++ kannax/plugins/fun/kang.py
@@ -42,9 +42,10 @@ async def kang_(message: Message):
     """kang um sticker"""
     user = await kannax.get_me()
     replied = message.reply_to_message
-    photo = None
+    media = None
     emoji_ = None
     is_anim = False
+    is_video = False
     resize = False
     if replied and replied.media:
         if replied.photo:
@@ -53,23 +54,34 @@ async def kang_(message: Message):
             resize = True
         elif replied.document and "tgsticker" in replied.document.mime_type:
             is_anim = True
+        elif (replied.document and "video" in replied.document.mime_type
+                and replied.document.file_size <= 10485760):
+            resize = True
+            is_video = True
+        elif replied.animation:
+            resize = True
+            is_video = True
         elif replied.sticker:
             if not replied.sticker.file_name:
                 await message.edit("`O sticker não tem nome!`")
                 return
             emoji_ = replied.sticker.emoji
             is_anim = replied.sticker.is_animated
-            if not replied.sticker.file_name.endswith(".tgs"):
+            is_video = replied.sticker.is_video
+            if not (
+                replied.sticker.file_name.endswith('.tgs')
+                or replied.sticker.file_name.endswith('.webm')
+            ):
                 resize = True
         else:
             await message.edit("`Arquivo não suportado!`")
             return
         await message.edit(f"`{random.choice(KANGING_STR)}`")
-        photo = await kannax.download_media(message=replied, file_name=Config.DOWN_PATH)
+        media = await kannax.download_media(message=replied, file_name=Config.DOWN_PATH)
     else:
         await message.edit("`Eu não posso roubar isso...`")
         return
-    if photo:
+    if media:
         args = message.filtered_input_str.split()
         pack = 1
         if len(args) == 2:
@@ -94,11 +106,15 @@ async def kang_(message: Message):
         packnick = f"{custom_packnick} Vol.{pack}"
         cmd = "/newpack"
         if resize:
-            photo = resize_photo(photo)
+            media = resize_photo(media, is_video)
         if is_anim:
             packname += "_anim"
             packnick += " (Animated)"
             cmd = "/newanimated"
+        if is_video:
+            packname += "_video"
+            packnick += " (Video)"
+            cmd = '/newvideo'
         exist = False
         try:
             exist = await message.client.send(
@@ -116,7 +132,7 @@ async def kang_(message: Message):
                 await conv.get_response(mark_read=True)
                 await conv.send_message(packname)
                 msg = await conv.get_response(mark_read=True)
-                limit = "50" if is_anim else "120"
+                limit = "50" if (is_anim or is_video) else "120"
                 while limit in msg.text:
                     pack += 1
                     packname = f"a{user.id}_by_kannax_{pack}"
@@ -124,6 +140,9 @@ async def kang_(message: Message):
                     if is_anim:
                         packname += "_anim"
                         packnick += " (Animated)"
+                    if is_video:
+                        packname += "_video"
+                        packnick += " (Video)"
                     await message.edit(
                         "`Mudando para Pack "
                         + str(pack)
@@ -136,7 +155,7 @@ async def kang_(message: Message):
                         await conv.get_response(mark_read=True)
                         await conv.send_message(packnick)
                         await conv.get_response(mark_read=True)
-                        await conv.send_document(photo)
+                        await conv.send_document(media)
                         await conv.get_response(mark_read=True)
                         await conv.send_message(emoji_)
                         await conv.get_response(mark_read=True)
@@ -161,7 +180,7 @@ async def kang_(message: Message):
                                 f"**Sticker** {out} __em um pacote diferente__**!**"
                             )
                         return
-                await conv.send_document(photo)
+                await conv.send_document(media)
                 rsp = await conv.get_response(mark_read=True)
                 if "Desculpe, o tipo de arquivo é inválido." in rsp.text:
                     await message.edit(
@@ -179,12 +198,12 @@ async def kang_(message: Message):
                 try:
                     await conv.send_message(cmd)
                 except YouBlockedUser:
-                    await message.edit("primeiro **desbloqueie** @Stickers")
+                    await message.edit("`Primeiro **desbloqueie** @Stickers`")
                     return
                 await conv.get_response(mark_read=True)
                 await conv.send_message(packnick)
                 await conv.get_response(mark_read=True)
-                await conv.send_document(photo)
+                await conv.send_document(media)
                 rsp = await conv.get_response(mark_read=True)
                 if "Desculpe, o tipo de arquivo é inválido." in rsp.text:
                     await message.edit(
@@ -212,8 +231,8 @@ async def kang_(message: Message):
                 else f"[roubado](t.me/addstickers/{packname})"
             )
             await message.edit(f"**Sticker** {out}**!**")
-        if os.path.exists(str(photo)):
-            os.remove(photo)
+        if os.path.exists(str(media)):
+            os.remove(media)
 
 
 @kannax.on_cmd(
@@ -255,9 +274,16 @@ async def sticker_pack_info_(message: Message):
     await message.edit(out_str)
 
 
-def resize_photo(photo: str) -> io.BytesIO:
-    """Redimensione a foto fornecida para 512x512"""
-    image = Image.open(photo)
+async def resize_media(media: str, video: bool) -> str:
+    """ Resize the given media to 512x512 """
+    if video:
+        resized_video = f"{media}.webm"
+        cmd = f"ffmpeg -i {media} -ss 00:00:00 -to 00:00:03 -map 0:v" + \
+            f" -c:v libvpx-vp9 -vf scale=512:512,fps=fps=30 {resized_video}"
+        await runcmd(cmd)
+        os.remove(media)
+        return resized_video
+    image = Image.open(media)
     maxsize = 512
     scale = maxsize / max(image.width, image.height)
     new_size = (int(image.width * scale), int(image.height * scale))
@@ -265,7 +291,7 @@ def resize_photo(photo: str) -> io.BytesIO:
     resized_photo = io.BytesIO()
     resized_photo.name = "sticker.png"
     image.save(resized_photo, "PNG")
-    os.remove(photo)
+    os.remove(media)
     return resized_photo
 
 
