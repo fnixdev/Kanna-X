1d5f5281797e39c5ecaabebbf4a82519dedece87
fnixdev <luisgatn000@gmail.com>
2022-04-18 11:59:38 -0300

    +

--- kannax/plugins/utils/speedtest.py
+++ kannax/plugins/utils/speedtest.py
@@ -15,41 +15,36 @@ CHANNEL = kannax.getCLogger(__name__)
 
 @kannax.on_cmd("speedtest", about={"header": "teste a velocidade de seu servidor"})
 async def speedtst(message: Message):
-    await message.edit("`Rodando speed test . . .`")
+    await message.edit("`Running speed test . . .`")
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        await message.try_to_edit("`Fazendo teste de download . . .`")
+        await message.try_to_edit("`Performing download test . . .`")
         test.download()
-        await message.try_to_edit("`Fazendo teste de upload . . .`")
+        await message.try_to_edit("`Performing upload test . . .`")
         test.upload()
         result = test.results.dict()
     except Exception as e:
-        await message.err(text=e)
+        await message.err(e)
         return
-    path = wget.download(result["share"])
-    output = f"""**--Iniciado as {result['timestamp']}--
-
-Cliente:
-
+    path = await pool.run_in_thread(wget.download)(result['share'])
+    output = f"""**--Started at {result['timestamp']}--
+Client:
 ISP: `{result['client']['isp']}`
-País: `{result['client']['country']}`
-
-Servidor:
-
-Nome: `{result['server']['name']}`
-País: `{result['server']['country']}, {result['server']['cc']}`
+Country: `{result['client']['country']}`
+Server:
+Name: `{result['server']['name']}`
+Country: `{result['server']['country']}, {result['server']['cc']}`
 Sponsor: `{result['server']['sponsor']}`
-Latencia: `{result['server']['latency']}`
-
+Latency: `{result['server']['latency']}`
 Ping: `{result['ping']}`
-Enviado: `{humanbytes(result['bytes_sent'])}`
-Recebido: `{humanbytes(result['bytes_received'])}`
+Sent: `{humanbytes(result['bytes_sent'])}`
+Received: `{humanbytes(result['bytes_received'])}`
 Download: `{humanbytes(result['download'] / 8)}/s`
 Upload: `{humanbytes(result['upload'] / 8)}/s`**"""
-    msg = await message.client.send_photo(
-        chat_id=message.chat.id, photo=path, caption=output
-    )
+    msg = await message.client.send_photo(chat_id=message.chat.id,
+                                          photo=path,
+                                          caption=output)
     await CHANNEL.fwd_msg(msg)
     os.remove(path)
     await message.delete()
