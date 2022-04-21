cda4180b04326c154ba2541e9117cc9c96ddceb4
fnix <49885720+fnixdev@users.noreply.github.com>
2022-04-21 01:17:57 -0300

    Update ubuntu.sh

--- resources/scripts/ubuntu.sh
+++ resources/scripts/ubuntu.sh
@@ -16,7 +16,7 @@ if [ ! -n "`which sudo`" ]; then
 fi
 
 sudo apt-get update -y
-sudo apt-get install tree wget2 p7zip-full jq ffmpeg wget git neofetch -y
+sudo apt-get install tree wget2 p7zip-full jq ffmpeg wget git neofetch mediainfo -y
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm google-chrome-stable_current_amd64.deb
